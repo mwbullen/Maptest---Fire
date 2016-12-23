d985895488a0fac357ab8bc586efcb173bc82a3c
mwbullen <Mike@new-host-2.home>
2016-12-23 18:50:00 -0500

    reimplemented plane movement in script

---
+++ MapTest/Assets/planeSpeed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeSpeed : MonoBehaviour {
+
+	public float airSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (0, 0, airSpeed * Time.deltaTime);
+
+	}
+}
---
+++ MapTest/Assets/planeSteering.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeSteering : MonoBehaviour {
+
+	public float baseTurnRate = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			steerPlane(baseTurnRate);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			steerPlane(baseTurnRate *-1);
+		}
+	}
+
+	void steerPlane(float turnRate) {
+		transform.Rotate (new Vector3 (0, turnRate, 0));
+
+	}
+		
+}

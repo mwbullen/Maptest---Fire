93bb82a172e5589e4ba76ed1be79644f2aa0eb7d
mwbullen <mwbullen@gmail.com>
2016-12-24 14:52:33 -0500

    adding tile clearing with space bar

---
+++ MapTest/Assets/planeActions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeActions : MonoBehaviour {
+	GameObject gameControl;
+	// Use this for initialization
+	void Start () {
+		gameControl = GameObject.FindGameObjectWithTag ("GameControl");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			clearTileBelow ();
+
+
+
+		}
+	}
+
+	void clearTileBelow() {
+		int aboveTileID = gameObject.GetComponent<planeTileTracking> ().aboveTileID;
+
+		GameObject aboveTile = gameControl.GetComponent<MapStatus> ().getTileatIndex (aboveTileID);
+
+		//can't clear tile if burning
+		if (aboveTile != null && aboveTile.GetComponent<TileInfo> ().onFire == false) {
+			gameControl.GetComponent<MapStatus> ().updateTileAtIndex (aboveTileID, '_');
+		}
+
+
+	}
+}

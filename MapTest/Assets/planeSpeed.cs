e8be85db5e7e1711692ae766efcc8a2a09a91c1d
mwbullen <mwbullen@gmail.com>
2016-12-26 11:25:34 -0500

    disabling turbo for now

--- MapTest/Assets/planeSpeed.cs
+++ MapTest/Assets/planeSpeed.cs
@@ -6,21 +6,28 @@ public class planeSpeed : MonoBehaviour {
 	public float baseAirSpeed = 10f;
 	public float turboModifier = 1.5f;
 
+	public float turboFuelCapacity;
+	public float currentTurboFuel;
 
 	// Use this for initialization
 	void Start () {
-
+		currentTurboFuel = turboFuelCapacity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		float airSpeed = baseAirSpeed;
+		/*
 
-		if (Input.GetKey(KeyCode.LeftShift)) {
+
+		if (Input.GetKey (KeyCode.LeftShift) && currentTurboFuel > 0) {
+			currentTurboFuel -= Time.deltaTime;
 			airSpeed *= turboModifier;
-		}
 
+		} else {
+			currentTurboFuel = Mathf.Clamp (currentTurboFuel + (Time.deltaTime), 0, turboFuelCapacity);				
+		}
+		*/
 		gameObject.transform.Translate (0, 0, airSpeed * Time.deltaTime);
 
 	}
